9466cb040343e09f06f920d475bf4808387927dc
Rikard Virta <rikard.virta90@gmail.com>
2020-11-13 09:56:31 +0100

    added game service interface

--- initial/src/main/java/com/example/restservice/GameService.java
+++ initial/src/main/java/com/example/restservice/GameService.java
@@ -1,44 +1,7 @@
 package com.example.restservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.context.annotation.SessionScope;
-
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-@SessionScope
-public class GameService {
-
-    @Autowired
-    GameLogic gameLogic;
-    String answer;
-    List<Feedback> history;
-
-    @PostConstruct
-    void initiate() {
-        generateAnswer();
-        history = new ArrayList<>();
-    }
-
-    public List<Feedback> makeGuess(String guess) {
-        String feedback = gameLogic.getFeedback(answer, guess);
-        boolean isCorrect = validate(guess);
-        if (isCorrect) {
-            generateAnswer();
-        }
-        history.add(new Feedback(guess, feedback, isCorrect));
-        return history;
-    }
-
-    private void generateAnswer() {
-        answer = gameLogic.generateAnswer();
-        System.out.println("Generated answer: " + answer);
-    }
-
-    boolean validate(String guess) {
-        return guess.equals(answer);
-    }
+public interface GameService {
+    List<Feedback> makeGuess(String guess);
 }
---
+++ initial/src/main/java/com/example/restservice/GameServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.restservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@SessionScope
+public class GameServiceImpl implements GameService {
+
+    @Autowired
+    GameLogic gameLogic;
+    String answer;
+    List<Feedback> history;
+
+    @PostConstruct
+    void initiate() {
+        generateAnswer();
+        history = new ArrayList<>();
+    }
+
+    @Override
+    public List<Feedback> makeGuess(String guess) {
+        String feedback = gameLogic.getFeedback(answer, guess);
+        boolean isCorrect = validate(guess);
+        if (isCorrect) {
+            generateAnswer();
+        }
+        history.add(new Feedback(guess, feedback, isCorrect));
+        return history;
+    }
+
+    private void generateAnswer() {
+        answer = gameLogic.generateAnswer();
+        System.out.println("Generated answer: " + answer);
+    }
+
+    boolean validate(String guess) {
+        return guess.equals(answer);
+    }
+}
